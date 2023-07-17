d7aa0203839d6d48447f8b70bb1d430ea743a5c1
vuonglen <leen.12621@gmail.com>
2023-07-17 18:15:11 +0700

    Vuong Len nop bai ss13

---
+++ ss7_bien_kieu_lt_toan_tu/thuchanh/bước2.js
@@ -0,0 +1,8 @@
+inputwidth = prompt("enter the width");
+inputheight = prompt(" enter the height");
+let width = parseInt(inputwidth);
+let height = parseInt(inputheight);
+let area = width * height ;
+document.write("the area is : " + area);
+// document.write("the area is:" + area);
+
